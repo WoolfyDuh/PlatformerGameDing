0da159fda049e3479f5246b411a7f4ad9461a0ae
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-04-05 11:53:58 +0200

    eerste piuuush
    unity files toegevoegd n alles

---
+++ 2D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{ 
+    public float speed;
+    public float rotateSpeed;
+    public float jumpingHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpingHeight = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * 10);// += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.down * rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += transform.forward * -speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Space))
+            {
+            transform.position += transform.up * jumpingHeight * Time.deltaTime;
+           }
+    }
+}
